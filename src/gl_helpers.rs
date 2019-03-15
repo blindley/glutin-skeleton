ebb2fc56e63374175d7342a11f5e587ff162f007
Benjamin Lindley <benjameslindley@gmail.com>
2019-03-15 17:49:42 -0500

    triangle

--- src/gl_helpers.rs
+++ src/gl_helpers.rs
@@ -3,31 +3,9 @@
 use std;
 use gl;
 
-#[allow(unused_imports)]
-use gl::types::{GLuint, GLint, GLsizei, GLchar, GLenum, GLsizeiptr, GLvoid};
-
 type BoxResult<T> = std::result::Result<T, Box<std::error::Error>>;
-pub type Error = simple_error::SimpleError;
+pub type Error = Box<std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
-
-macro_rules! assert_or_error {
-    ($cond:expr) => (
-        if !$cond {
-            let msg = format!("assertion failed: {}", stringify!($cond));
-            Err(Error::new(msg))
-        } else {
-            Ok(())
-        }
-    );
-    ($cond:expr, $($arg:tt)+) => (
-        if !$cond {
-            let msg = format!($($arg)+);
-            Err(Error::new(msg))
-        } else {
-            Ok(())
-        }
-    );
-}
 
 pub struct ProgramBuilder {
     code: ShaderCode,
@@ -54,6 +32,34 @@ impl ProgramBuilder {
         self
     }
 
+    pub fn geometry_shader_code<T>(&mut self, code : T) -> &mut ProgramBuilder
+        where T : Into<String>
+    {
+        self.code[ShaderType::Geometry] = Some(code.into());
+        self
+    }
+
+    pub fn tess_control_shader_code<T>(&mut self, code : T) -> &mut ProgramBuilder
+        where T : Into<String>
+    {
+        self.code[ShaderType::TessControl] = Some(code.into());
+        self
+    }
+
+    pub fn tess_eval_shader_code<T>(&mut self, code : T) -> &mut ProgramBuilder
+        where T : Into<String>
+    {
+        self.code[ShaderType::TessEval] = Some(code.into());
+        self
+    }
+
+    pub fn compute_shader_code<T>(&mut self, code : T) -> &mut ProgramBuilder
+        where T : Into<String>
+    {
+        self.code[ShaderType::Compute] = Some(code.into());
+        self
+    }
+
     pub fn code(&mut self, code : &ShaderCode) -> &mut ProgramBuilder {
         for i in ShaderType::each().iter() {
             if let Some(ref code) = code[*i] {
@@ -63,7 +69,7 @@ impl ProgramBuilder {
         self
     }
 
-    pub fn build(&self) -> Result<GLuint> {
+    pub fn build(&self) -> Result<u32> {
         let mut shaders = Vec::new();
         for shader_type in ShaderType::each().iter() {
             if let Some(ref code) = self.code[*shader_type] {
@@ -75,12 +81,12 @@ impl ProgramBuilder {
     }
 }
 
-pub fn compile_shader(code : &str, shader_type : ShaderType) -> Result<GLuint> {
+pub fn compile_shader(code : &str, shader_type : ShaderType) -> Result<u32> {
     unsafe {
         let gl_type = shader_type.as_gl_enum();
         let shader = gl::CreateShader(gl_type);
-        let len = code.len() as GLint;
-        let code_ptr = code.as_ptr() as *const GLchar;
+        let len = code.len() as i32;
+        let code_ptr = code.as_ptr() as *const std::os::raw::c_char;
         gl::ShaderSource(shader, 1, &code_ptr, &len);
         gl::CompileShader(shader);
 
@@ -92,7 +98,7 @@ pub fn compile_shader(code : &str, shader_type : ShaderType) -> Result<GLuint> {
             let buffer_len = (loglen - 1) as usize;
             let mut buffer : Vec<u8> = Vec::with_capacity(buffer_len + 1);
             gl::GetShaderInfoLog(shader, loglen, std::ptr::null_mut(),
-                buffer.as_ptr() as *mut GLchar);
+                buffer.as_ptr() as *mut std::os::raw::c_char);
             buffer.set_len(loglen as usize - 1);
             let log = String::from_utf8(buffer).unwrap();
             
@@ -100,15 +106,15 @@ pub fn compile_shader(code : &str, shader_type : ShaderType) -> Result<GLuint> {
             let message = format!("error in {} shader : {}", shader_name, log);
 
             gl::DeleteShader(shader);
-            Err(Error::new(message))
+            Err(message.into())
         } else {
             Ok(shader)
         }
     }
 }
 
-pub fn create_program(shaders : &[GLuint], delete_shaders : bool)
-    -> Result<GLuint>
+pub fn create_program(shaders : &[u32], delete_shaders : bool)
+    -> Result<u32>
 {
     unsafe {
         let program = gl::CreateProgram();
@@ -131,11 +137,11 @@ pub fn create_program(shaders : &[GLuint], delete_shaders : bool)
             let buffer_len = (loglen - 1) as usize;
             let mut buffer : Vec<u8> = Vec::with_capacity(buffer_len + 1);
             gl::GetProgramInfoLog(program, loglen, std::ptr::null_mut(),
-                buffer.as_ptr() as *mut GLchar);
+                buffer.as_ptr() as *mut std::os::raw::c_char);
             buffer.set_len(loglen as usize - 1);
             let message = String::from_utf8(buffer).unwrap();
             gl::DeleteProgram(program);
-            Err(Error::new(message))
+            Err(message.into())
         } else {
             Ok(program)
         }
@@ -149,44 +155,53 @@ pub enum BufferUsage {
     DynamicDraw, DynamicRead, DynamicCopy,
 }
 
-pub fn create_buffer<T>(data : &[T], usage: BufferUsage) -> Result<GLuint> {
+impl BufferUsage {
+    pub fn as_gl_enum(self) -> u32 {
+        match self {
+            BufferUsage::StreamDraw => gl::STREAM_DRAW,
+            BufferUsage::StreamRead => gl::STREAM_READ,
+            BufferUsage::StreamCopy => gl::STREAM_COPY,
+            BufferUsage::StaticDraw => gl::STATIC_DRAW,
+            BufferUsage::StaticRead => gl::STATIC_READ,
+            BufferUsage::StaticCopy => gl::STATIC_COPY,
+            BufferUsage::DynamicDraw => gl::DYNAMIC_DRAW,
+            BufferUsage::DynamicRead => gl::DYNAMIC_READ,
+            BufferUsage::DynamicCopy => gl::DYNAMIC_COPY,
+        }
+    }
+}
+
+pub fn create_buffer<T>(data : &[T], usage: BufferUsage) -> Result<u32> {
     unsafe {
         let mut buffer = std::mem::uninitialized();
         gl::GenBuffers(1, &mut buffer);
-        let size = (data.len() * std::mem::size_of::<T>()) as GLsizeiptr;
+        // named_buffer_data(buffer, data, usage);
         gl::BindBuffer(gl::ARRAY_BUFFER, buffer);
-
-        use self::BufferUsage::*;
-        let gl_usage = match usage {
-            StreamDraw => gl::STREAM_DRAW,
-            StreamRead => gl::STREAM_READ,
-            StreamCopy => gl::STREAM_COPY,
-            StaticDraw => gl::STATIC_DRAW,
-            StaticRead => gl::STATIC_READ,
-            StaticCopy => gl::STATIC_COPY,
-            DynamicDraw => gl::DYNAMIC_DRAW,
-            DynamicRead => gl::DYNAMIC_READ,
-            DynamicCopy => gl::DYNAMIC_COPY,
-        };
-
-        gl::BufferData(gl::ARRAY_BUFFER, size, data.as_ptr() as _,
-            gl_usage);
+        let size = (data.len() * std::mem::size_of::<T>()) as isize;
+        gl::BufferData(gl::ARRAY_BUFFER, size, data.as_ptr() as _, usage.as_gl_enum());
         Ok(buffer)
     }
 }
 
-pub fn create_single_buffer_vertex_array(buffer : GLuint, components : &[GLint]) -> Result<GLuint> {
+pub fn named_buffer_data<T>(buffer: u32, data: &[T], usage: BufferUsage) {
+    let size = (data.len() * std::mem::size_of::<T>()) as isize;
+    unsafe {
+        gl::NamedBufferData(buffer, size, data.as_ptr() as _, usage.as_gl_enum());
+    }
+}
+
+pub fn create_single_buffer_vertex_array(buffer : u32, components : &[i32]) -> Result<u32> {
     unsafe {
         let mut vertex_array = std::mem::uninitialized();
         gl::GenVertexArrays(1, &mut vertex_array);
         gl::BindVertexArray(vertex_array);
         gl::BindBuffer(gl::ARRAY_BUFFER, buffer);
-        let total_components : GLsizei = components.iter().sum();
-        let stride = total_components * std::mem::size_of::<f32>() as GLsizei;
+        let total_components : i32 = components.iter().sum();
+        let stride = total_components * std::mem::size_of::<f32>() as i32;
         let mut offset : usize = 0;
         for (index, &comp) in components.iter().enumerate() {
-            let index = index as GLuint;
-            let offset_ptr = offset as *mut GLvoid;
+            let index = index as u32;
+            let offset_ptr = offset as *mut std::os::raw::c_void;
             gl::VertexAttribPointer(index, comp, gl::FLOAT, gl::FALSE,
                 stride, offset_ptr);
             gl::EnableVertexAttribArray(index);
@@ -261,8 +276,11 @@ pub fn get_attribute_location(program: u32, name: *const std::os::raw::c_char)
 -> Result<i32> {
     unsafe {
         let loc = gl::GetAttribLocation(program, name);
-        assert_or_error!(loc != -1, "could not bind attribute {:?}", std::ffi::CStr::from_ptr(name))?;
-        Ok(loc)
+        if loc == -1 {
+            Err(format!("could not bind attribute {:?}", std::ffi::CStr::from_ptr(name)).into())
+        } else {
+            Ok(loc)
+        }
     }
 }
 
@@ -270,7 +288,10 @@ pub fn get_uniform_location(program: u32, name: *const std::os::raw::c_char)
 -> Result<i32> {
     unsafe {
         let loc = gl::GetUniformLocation(program, name);
-        assert_or_error!(loc != -1, "could not bind uniform {:?}", std::ffi::CStr::from_ptr(name))?;
-        Ok(loc)
+        if loc == -1 {
+            Err(format!("could not bind uniform {:?}", std::ffi::CStr::from_ptr(name)).into())
+        } else {
+            Ok(loc)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-extern crate glutin;
-extern crate gl;
-extern crate simple_error;
+use glutin;
+use gl;
 
 mod gl_helpers;
 
 fn main() -> Result<(),Box<std::error::Error>> {
     use glutin::GlContext;
     let window_title = "nice window title";
-    let window_size = (800, 800);
+    let window_size = (800,800);
+    let gl_version = (4,3);
 
     let mut ev_loop = glutin::EventsLoop::new();
     let gl_window = {
         let window = glutin::WindowBuilder::new()
             .with_title(window_title)
             .with_dimensions(window_size.into());
+        let gl_request = glutin::GlRequest::Specific(
+            glutin::Api::OpenGl, gl_version);
         let context = glutin::ContextBuilder::new()
+            .with_gl_profile(glutin::GlProfile::Core)
+            .with_gl(gl_request)
             .with_vsync(true);
         glutin::GlWindow::new(window, context, &ev_loop)?
     };
@@ -22,7 +26,40 @@ fn main() -> Result<(),Box<std::error::Error>> {
     unsafe {
         gl_window.make_current()?;
         gl::load_with(|sym| gl_window.get_proc_address(sym) as *const _);
+        gl::Enable(gl::DEBUG_OUTPUT);
+        gl::DebugMessageCallback(gl_debug_callback, std::ptr::null_mut());
     }
+
+    let program = {
+        let code = {
+            let file = std::fs::File::open("res/shader.multitext")?;
+            let file = std::io::BufReader::new(file);
+            use std::io::BufRead;
+            multitext::parse_lines(file.lines().filter_map(|r| r.ok()))?
+        };
+
+        let vcode = code.get("vertex shader").expect("no vertex shader found").clone();
+        let fcode = code.get("fragment shader").expect("no fragment shader found").clone();
+        gl_helpers::ProgramBuilder::new()
+            .vertex_shader_code(vcode)
+            .fragment_shader_code(fcode)
+            .build()?
+    };
+
+    let buffer = {
+        let vertices: [f32;15] = [
+            0., 0.8, 1.0, 0.0, 0.0,
+            0.8, -0.8, 0.0, 1.0, 0.0,
+            -0.8, -0.8, 0.0, 0.0, 1.0,
+        ];
+        gl_helpers::create_buffer(&vertices, gl_helpers::BufferUsage::StaticDraw)?
+    };
+
+    println!("buffer = {}", buffer);
+
+    let vao = gl_helpers::create_single_buffer_vertex_array(buffer, &[2,3])?;
+
+    println!("{:?}", (program, buffer, vao));
 
     // let start_time = std::time::Instant::now();
 
@@ -30,30 +67,51 @@ fn main() -> Result<(),Box<std::error::Error>> {
     while running {
         ev_loop.poll_events(|event| {
             match event {
-                glutin::Event::WindowEvent{event, ..} => {
-                    match event {
-                        glutin::WindowEvent::CloseRequested => running = false,
+                glutin::Event::WindowEvent{event, ..} => match event {
+                    glutin::WindowEvent::CloseRequested => running = false,
 
-                        glutin::WindowEvent::Resized(logical_size) => {
-                            let dpi_factor = gl_window.get_hidpi_factor();
-                            gl_window.resize(logical_size.to_physical(dpi_factor));
-                            unsafe {
-                                let w = logical_size.width as i32;
-                                let h = logical_size.height as i32;
-                                gl::Viewport(0, 0, w, h);
-                            }
-                        },
+                    glutin::WindowEvent::Resized(logical_size) => {
+                        let dpi_factor = gl_window.get_hidpi_factor();
+                        gl_window.resize(logical_size.to_physical(dpi_factor));
+                        unsafe {
+                            let w = logical_size.width as i32;
+                            let h = logical_size.height as i32;
+                            gl::Viewport(0, 0, w, h);
+                        }
+                    },
 
-                        _ => (),
-                    }
+                    _ => (),
                 },
 
                 _ => (),
             }
         });
 
+        unsafe {
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::UseProgram(program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
         gl_window.swap_buffers()?;
     }
 
     Ok(())
+}
+
+extern "system" fn gl_debug_callback(source: gl::types::GLenum, ty: gl::types::GLenum, id: u32,
+    severity: gl::types::GLenum, length: i32,
+    message: *const std::os::raw::c_char,
+    user_param: *mut std::os::raw::c_void)
+{
+    let _ = (source, ty, id, severity, user_param);
+
+    unsafe {
+        let message = std::slice::from_raw_parts(message as *const u8, length as usize);
+        let message = std::str::from_utf8(message).expect("bad opengl error message");
+        println!("{}", message);
+    }
 }
